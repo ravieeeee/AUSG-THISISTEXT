37a5b4d44706f61b3f00a53bac8dc0568f79fbf1
JuYeong <pyeonjy97@gmail.com>
2019-05-24 00:25:42 +0900

    update App.js

--- image-to-text/src/App.js
+++ image-to-text/src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import Form from './components/Form'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  state = {
+    isLoading: false,
+    result: ''
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.isLoading ?
+          <div>Loading....</div> : 
+          <Form
+            getResult={this.getResult}
+            setLoadingStatus={this.setLoadingStatus}
+          />
+        }
+        {this.state.result !== '' ?
+          <div>{this.state.result}</div> : null
+        }
+      </div>
+    )
+  }
+
+  setLoadingStatus = (status) => {
+    this.setState({
+      isLoading: status
+    })
+  }
+
+  getResult = (result) => {
+    this.setState({
+      result
+    })
+  }
 }
-
-export default App;
